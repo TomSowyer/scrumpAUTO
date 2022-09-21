6e6e5f3b00bfd95b6a85fc125e6f3e19157a59d6
TomSowyer <tomsowyertm@gmail.com>
2022-09-21 16:27:48 +0300

    replace chromedriver

--- регистрация/registration test.py
+++ регистрация/registration test.py
@@ -5,7 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-driver = webdriver.Chrome('/Users/dimaprisepov/Downloads/chromedriver 5') 
+import os 
+driver = webdriver.Chrome()
 
 driver.get('https://dev.mineplex.io/ru')
 time.sleep(3)
