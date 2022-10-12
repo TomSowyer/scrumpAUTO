c9f18888620bffa4291f614fbdb2665a49865e03
TomSowyer <tomsowyertm@gmail.com>
2022-10-12 16:27:25 +0300

    registration refactor

--- регистрация/registration test.py
+++ регистрация/registration test.py
@@ -9,28 +9,27 @@ import os
 driver = webdriver.Chrome()
 
 driver.get('https://dev.mineplex.io/ru')
-time.sleep(3)
-regLink = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div/div[1]/div[1]/div[2]/div/button[2]')
-regLink.click()
-time.sleep(2)
-namefield = driver.find_element_by_xpath('//*[@id="loginForm_firstName"]')
-Lnamefield = driver.find_element_by_xpath('//*[@id="loginForm_lastName"]')
-Passfield = driver.find_element_by_xpath('//*[@id="loginForm_password"]')
-twoPassfield = driver.find_element_by_xpath('//*[@id="loginForm_confirmPassword"]')
+driver.implicitly_wait(15)
+refID = driver.find_element_by_css_selector('#passwordForDev_devPassword').send_keys('Gw1pqh5yxQ')
+refID_Button = driver.find_element_by_css_selector('#passwordForDev > div.MuiBox-root.jss101 > button').click()
+driver.implicitly_wait(15)
+regLink = WebDriverWait(driver, 15).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div/div/div/div[1]/div[1]/div[2]/div/button[2]'))
+).click()
 
-namefield.send_keys('auto')
-Lnamefield.send_keys('user dima')
-Passfield.send_keys('DanyDany@42198')
-twoPassfield.send_keys('DanyDany@42198')
+time.sleep(2)
+namefield = driver.find_element_by_xpath('//*[@id="loginForm_firstName"]').send_keys('auto')
+Lnamefield = driver.find_element_by_xpath('//*[@id="loginForm_lastName"]').send_keys('user dima')
+Passfield = driver.find_element_by_xpath('//*[@id="loginForm_password"]').send_keys('DanyDany@42198')
+twoPassfield = driver.find_element_by_xpath('//*[@id="loginForm_confirmPassword"]').send_keys('DanyDany@42198')
 
 newwindow = 'window.open("https://tempmail.plus/ru")'
 driver.execute_script(newwindow)
  # Переместите ручку для работы на вновь открытой странице
 driver.switch_to.window(driver.window_handles[1])
-time.sleep(3)
-copy = driver.find_element_by_xpath('//*[@id="pre_copy"]')
-copy.click()
-time.sleep(1)
+driver.implicitly_wait(15)
+copy = driver.find_element_by_xpath('//*[@id="pre_copy"]').click()
+driver.implicitly_wait(15)
 
 x = pyperclip.paste()
 print(x)
@@ -44,8 +43,7 @@ bot.send_message(CHANNEL_NAME, text)
 
 driver.switch_to.window(driver.window_handles[0])
 
-LoginField = driver.find_element_by_xpath('//*[@id="loginForm_email"]')
-LoginField.send_keys(x)
+LoginField = driver.find_element_by_xpath('//*[@id="loginForm_email"]').send_keys(x)
 
 checkOne = driver.find_element_by_xpath('/html/body/div/div/div/div[1]/div[2]/div/div/div/div[2]/form/fieldset/div/fieldset[1]/label/span[1]/span/input').click()
 checkOne = driver.find_element_by_xpath('/html/body/div/div/div/div[1]/div[2]/div/div/div/div[2]/form/fieldset/div/fieldset[2]/label/span[1]/span/input').click()
@@ -57,8 +55,8 @@ myMail = WebDriverWait(driver, 15).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "#container-body > div.container-xl.body > div.inbox > div.mail"))
 )
 myMail.click()
-time.sleep(3)
-gogogo = driver.find_element_by_css_selector('tr:nth-child(3) > td > div > a > font').click()
+gogogo = WebDriverWait(driver, 15).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'tr:nth-child(3) > td > div > a > font'))
+).click()
 
-time.sleep(8)
-
+time.sleep(5)
